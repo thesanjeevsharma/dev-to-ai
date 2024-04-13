81bf503a17e5d13b1e55c66798c2da95f58ce3ca
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2024-04-13 11:42:22 +0530

    chore: add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>DEV AI ✨</title>
+        <meta
+          name="description"
+          content="Unofficial AI app for DEV readers and creators."
+        />
+      </Head>
       <body className="min-h-screen bg-zinc-950">
         <Main />
         <NextScript />
